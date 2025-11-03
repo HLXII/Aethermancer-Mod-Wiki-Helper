4d4713b0f8d85cb345c723b79318cdb36ccaa425
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2025-11-03 16:24:02 -0600

    Split out to separate files for management

---
+++ src/Models/ActionData.cs
@@ -0,0 +1,63 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WikiHelper.Models;
+
+
+public class ActionData
+{
+    public string Name { get; set; } = "";
+    public bool Starting { get; set; } = false;
+    public bool Maverick { get; set; } = false;
+    public bool Attack { get; set; } = false;
+    public bool Free { get; set; } = false;
+    public List<string> Types { get; set; } = new();
+    public List<string> Elements { get; set; } = new();
+    public string Requires { get; set; } = "";
+    public string Monster { get; set; } = "";
+    public string Effect { get; set; } = "";
+    public List<string> Key { get; set; } = new();
+    public List<string> Category { get; set; } = new();
+};
+
+public static class BaseActionExtensions
+{
+    public static ActionData ToData(this BaseAction action, Monster monster)
+    {
+        var data = new ActionData();
+        data.Name = action.Name;
+        data.Starting = action.IsStartingAction();
+        data.Maverick = action.MaverickSkill;
+        data.Attack = action.IsAttackingAction();
+        data.Free = action.IsFreeAction();
+        data.Types = action.GetSkillTypes();
+        data.Elements = action.Cost.ToList();
+        data.Requires = action.GetRequirement();
+        SkillPicker.WeightedSkill weightedSkill = new SkillPicker.WeightedSkill(action, 1);
+        SkillInstance skillInstance = new SkillInstance(weightedSkill.Action, monster);
+        data.Effect = ParseHelper.ReformatDescription(action.GetDescription(skillInstance));
+        try
+        {
+            data.Key = ParseHelper.ParseForKeys(data.Effect);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Failed to parse keys for {data.Name} -  \"{data.Effect}\" - {ex}");
+        }
+        return data;
+    }
+
+    public static bool IsStartingAction(this BaseAction action)
+    {
+        return action.Types.Any(type => type.GetComponent<MonsterType>().GetName() == "Starting Actions");
+    }
+
+    public static bool IsAttackingAction(this BaseAction action)
+    {
+        return !action.IsActionSubType(EActionSubType.NonDamagingAction);
+    }
+}
---
+++ src/Models/TraitData.cs
@@ -0,0 +1,83 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WikiHelper.Models;
+
+public class TraitData
+{
+    public string Name { get; set; } = "";
+    public bool Maverick { get; set; } = false;
+    public bool Aura { get; set; } = false;
+    public List<string> Types { get; set; } = new();
+    public string Requires { get; set; } = "";
+    public string Effect { get; set; } = "";
+    public List<string> Key { get; set; } = new();
+    public List<string> Category { get; set; } = new();
+};
+
+public class SigTraitData
+{
+    public string Name { get; set; } = "";
+    public bool Shifted { get; set; } = false;
+    public bool Aura { get; set; } = false;
+    public string Effect { get; set; } = "";
+    public List<string> Key { get; set; } = new();
+    public List<string> Category { get; set; } = new();
+};
+
+public static class TraitExtensions
+{
+    public static TraitData ToData(this Trait trait, Monster monster)
+    {
+        var data = new TraitData();
+        data.Name = trait.Name;
+        data.Maverick = trait.MaverickSkill;
+        data.Aura = trait.Aura;
+        data.Types = trait.GetSkillTypes();
+        data.Requires = trait.GetRequirement();
+
+        SkillPicker.WeightedSkill weightedSkill = new SkillPicker.WeightedSkill(trait, 1);
+        PassiveInstance passiveInstance = new PassiveInstance(weightedSkill.Skill, monster);
+        data.Effect = ParseHelper.ReformatDescription(trait.GetDescription(passiveInstance));
+        try
+        {
+            data.Key = ParseHelper.ParseForKeys(data.Effect);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Failed to parse keys for {data.Name} -  \"{data.Effect}\" - {ex}");
+        }
+        return data;
+    }
+
+    public static SigTraitData ToSigData(this Trait trait, Monster monster)
+    {
+        var data = new SigTraitData();
+        data.Name = trait.Name;
+        data.Shifted = trait.IsShiftedTrait;
+        data.Aura = trait.Aura;
+
+        SkillPicker.WeightedSkill weightedSkill = new SkillPicker.WeightedSkill(trait, 1);
+        PassiveInstance passiveInstance = new PassiveInstance(weightedSkill.Skill, monster);
+        data.Effect = ParseHelper.ReformatDescription(trait.GetDescription(passiveInstance));
+        try
+        {
+            data.Key = ParseHelper.ParseForKeys(data.Effect);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Failed to parse keys for {data.Name} -  \"{data.Effect}\" - {ex}");
+        }
+
+        return data;
+    }
+
+    public static bool IsSignatureTrait(this Trait trait)
+    {
+        return trait.Types.Any(type => type.GetComponent<MonsterType>().GetName() == "Signature Traits");
+    }
+}
